2076f9a69e656351930c2ae77df9151817d5f650
Sergej Radkovec <radkovecsergej@gmail.com>
2018-09-09 13:46:40 +0300

    add task2

--- lesson-3-basic-constructions/task2.js
+++ lesson-3-basic-constructions/task2.js
@@ -1,9 +1,39 @@
-var firstName = prompt('Введите вашe имя:', '');
-var secondName = prompt('Введите ваше отчество:', '');
-var lastName = prompt('Введите вашу фамилию:', '');
-var fullYears = parseInt(prompt('Сколько вам лет:', ''), 10);
-var gender = confirm('Ваш пол - мужской?');
+var firstName;
+var secondName;
+var lastName;
+var fullYears;
+var gender;
 var pension = false;
+
+do {
+  firstName = prompt('Введите вашe имя:', '');
+  if (firstName >= 0) {
+    alert('Введите пожалуйста корректные данные');
+  }
+} while (firstName >= 0);
+
+do {
+  secondName = prompt('Введите вашe отчество:', '');
+  if (secondName >= 0) {
+    alert('Введите пожалуйста корректные данные');
+  }
+} while (secondName >= 0);
+
+do {
+  lastName = prompt('Введите вашу фамилию', '');
+  if (lastName >= 0) {
+    alert('Введите пожалуйста корректные данные');
+  }
+} while (lastName >= 0);
+
+do {
+  fullYears = parseInt(prompt('Сколько вам лет:', ''), 10);
+  if (fullYears < 0 || fullYears > 150 || isNaN(fullYears)) {
+    alert('Введите пожалуйста корректные данные');
+  }
+} while (fullYears < 0 || fullYears > 150 || isNaN(fullYears));
+
+gender = confirm('Ваш пол - мужской?');
 
 if (gender && (fullYears >= 63)) {
   pension = true;
