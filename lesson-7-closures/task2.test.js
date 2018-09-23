079382e143a0d10e6fe87e911db3e57a52a1d57d
Sergej Radkovec <radkovecsergej@gmail.com>
2018-09-23 20:12:40 +0300

    Add test file

---
+++ lesson-7-closures/task2.test.js
@@ -0,0 +1,15 @@
+const interviewQuestion = require('./task1');
+
+describe('test task2 with different jobs', () => {
+  it('question for the teacher: What subject do you teach John?', () => {
+    expect(interviewQuestion('teacher')('John').toBe('What subject do you teach John?');
+  });
+
+  it('question for the designer: John can you please explain what UX design is?', () => {
+    expect(interviewQuestion('designer')('John').toBe('John can you please explain what UX design is?');
+  });
+
+  it('question for other jobs: Hello John, what do you do?', () => {
+    expect(interviewQuestion('driver')('John').toBe('Hello John, what do you do?');
+  });
+});
