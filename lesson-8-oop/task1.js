4fff1fac5f2b349f30e9f004d30424bbb8c152b7
Sergej Radkovec <radkovecsergej@gmail.com>
2018-09-25 18:45:51 +0300

    add some questions

--- lesson-8-oop/task1.js
+++ lesson-8-oop/task1.js
@@ -6,7 +6,9 @@ function Question(question, answers, correctAnswer) {
   this.correctAnswer = correctAnswer;
 }
 
-var questionCapital = new Question('Столица Беларуси?', ['Гродно', 'Минск', 'Брест'], 2);
+var question1 = new Question('Столица Беларуси?', ['Гродно', 'Минск', 'Брест'], 2);
+var question2 = new Question('Производитель смартфонов?', ['Груша', 'Персик', 'Яблоко'], 3);
+var question3 = new Question('Куда Маск отправил Теслу?', ['В космос', 'На дно океана', 'На марс'], 3);
 
-console.log(questionCapital);
+
 
