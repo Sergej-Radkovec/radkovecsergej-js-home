1e220646f5f9b0d634e30bed911252546abfb56f
Sergej Radkovec <radkovecsergej@gmail.com>
2018-09-26 21:46:53 +0300

    add random select function

--- lesson-8-oop/task1.js
+++ lesson-8-oop/task1.js
@@ -1,20 +1,25 @@
 'use strict';
-
+// p.1 Build a function constructor called Question
 function Question(question, answers, correctAnswer) {
   this.question = question;
   this.answers = answers;
   this.correctAnswer = correctAnswer;
 }
 
+// p.2 Create a couple of questions
 var question1 = new Question('Столица Беларуси?', ['Гродно', 'Минск', 'Брест'], 2);
 var question2 = new Question('Производитель смартфонов?', ['Груша', 'Персик', 'Яблоко'], 3);
 var question3 = new Question('Куда Маск отправил Теслу?', ['В космос', 'На дно океана', 'На марс'], 3);
 
+// p.3 Store all questions inside an array
 var questions = [question1, question2, question3];
 
+// p.4 Select one random question and log it on the console
+function selectOneRandom(questionsArray) {
+  return questionsArray[(Math.ceil(Math.random() * (questionsArray.length)) - 1)];
+}
 
-
-Question.prototype.askQuestion  = function () {
+Question.prototype.printQuestion  = function () {
   var strAnswers = '';
   for (var i = 1; i <= this.answers.length; i++) {
     strAnswers = strAnswers + i + ')' + this.answers[i - 1] + ';' + '\n';
@@ -22,15 +27,15 @@ Question.prototype.askQuestion  = function () {
   console.log(this.question + '\n' + strAnswers);
 };
 
-questions[2].askQuestion();
+// p.5 Use the 'prompt' function to ask the user for the correct answer
+var userAnswer = parseInt(prompt('Номер вашего варианта'), 10);
 
-Question.prototype.checkAnswers = function (userAnswers) {
-  if (userAnswers === this.correctAnswer) {
+// p.6 Check user's answer
+Question.prototype.checkAnswer = function (answer) {
+  if (answer === this.correctAnswer) {
     console.log('Ваш ответ верен');
   } else {
     console.log('Правильный ответ ' + this.correctAnswer);
   }
 };
-
-questions[2].checkAnswers(parseInt(prompt('Номер вашего варианта'), 10));
 
