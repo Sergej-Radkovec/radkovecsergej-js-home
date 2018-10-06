05fae8ff7df480e0f0ddc27f50708298f6177ba6
Sergej Radkovec <radkovecsergej@gmail.com>
2018-10-06 18:55:02 +0300

    add variable size on board

--- lesson-5-closures/task1.js
+++ lesson-5-closures/task1.js
@@ -1,18 +1,20 @@
 'use strict';
 
-function createChessBoard(size) {
+function createChessBoard(width, height) {
   var board = '';
-  for (var i = 1; i < size * size; i++) {
-    if ((i % (size + 1)) === 0) {
-      board += '\n';
-    } else if (i % 2 !== 0) {
-      board += '#';
-    } else {
-      board += ' ';
+  for (var i = 1; i < height; i++) {
+    for (var j = 0; j < width; j++) {
+      if ((i + j) % 2 === 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
     }
+    board += "\n";
   }
   return board;
 }
 
-console.log(createChessBoard(10));
+console.log(createChessBoard(9,5));
+console.log(createChessBoard(10,8));
 
