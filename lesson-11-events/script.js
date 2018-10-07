6f55dd571f148366ac29d97cf15815e1fd0e3b52
Sergej Radkovec <radkovecsergej@gmail.com>
2018-10-07 22:00:41 +0300

    add select

--- lesson-11-events/script.js
+++ lesson-11-events/script.js
@@ -10,30 +10,63 @@
     function addFormElement(obj) {
       var label = labelCreate(obj.name, obj.label);
       cloneForm.appendChild(label);
-      var input = inputCreate(obj.type, obj.name);
-      cloneForm.appendChild(input);
+      /*var input = inputCreate(obj.type, obj.name);
+      cloneForm.appendChild(input);*/
+      var select = selectCreate(obj);
+      cloneForm.appendChild(select);
+      addBr();
+    }
+
+    function addBr() {
+      var br = document.createElement('br');
+      cloneForm.appendChild(br);
     }
 
     function inputCreate(type, name) {
-      var input = doc.createElement('input');
-      input.setAttribute('name', name);
-      input.setAttribute('id', name);
-      return input;
+      if (type === 'text') {
+        var input = document.createElement('input');
+        input.setAttribute('name', name);
+        input.setAttribute('id', name);
+        return input;
+      }
     }
 
     function labelCreate(name, text) {
-      var label = doc.createElement('label');
+      var label = document.createElement('label');
       label.setAttribute('for', name);
       label.textContent = text;
       return label;
+    }
+
+    function selectCreate(obj) {
+      var select = document.createElement('select');
+      select.setAttribute('name', obj.name);
+      select.setAttribute('id', obj.name);
+      for (var i = 0; i < obj.variants.length; i++) {
+        var option = document.createElement('option');
+        option.setAttribute('value', obj.variants[i].value);
+        option.textContent = obj.variants[i].text;
+        select.appendChild(option);
+      }
+      return select;
     }
   }
 
   var doc = document;
   var container = doc.body;
   var origForm = doc.forms.addSiteForm;
-  var formFields = [{label: 'Разработчики:', type: 'text', width: 200}];
-
+  var formFields = [
+    /*{label: 'Разработчики:', type: 'text', width: 200},
+    {label: 'Название сайта:', type: 'text', width: 200},
+    {label: 'URL сайта:', type: 'text', width: 200},
+    {label: 'Дата запуска сайта:', type: 'text', width: 200},
+    {label: 'Посетителей в сутки:', type: 'text', width: 200},
+    {label: 'E-mail для связи:', type: 'text', width: 200},*/
+    {label: 'Рубрика каталога:', type: 'select',
+      variants: [
+        {text: 'здоровье', value: 'health'},
+        {text: 'домашний уют', value: 'comfort'},
+        {text: 'бытовая техника', value: 'appliances'}], width: 200}];
   generateForm(container, origForm, formFields);
 })();
 
