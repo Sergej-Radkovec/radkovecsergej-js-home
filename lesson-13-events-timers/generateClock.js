ac555d9f0afc99dfdb7a815e2dd92ecd7ee8240f
Sergej Radkovec <radkovecsergej@gmail.com>
2018-10-20 22:03:47 +0300

    division into small functions

--- lesson-13-events-timers/generateClock.js
+++ lesson-13-events-timers/generateClock.js
@@ -1,108 +1,105 @@
 'use strict';
-(function () {
-  function generateClock(parent, size, clockColor, sectionColor) {
-    var clock = doc.createElement('div');
-    clock.style.position = 'absolute';
-    clock.style.width = size + 'px';
-    clock.style.height = size + 'px';
-    clock.style.backgroundColor = clockColor;
-    clock.style.borderRadius = '50%';
-    container.appendChild(clock);
+var size = 355;
+var sizeCircle = size / 15;
+var radiusHour = size * 0.42;
+var baseColor = '#03a9f4';
+var sectionColor = 'red';
+var digitalWatchWidth = size / 1.7;
+var digitalWatchHeight = size / 5;
+var handMargin = size / 40;
+var hourHandBold = size / 50;
+var hourHandWidth = size / 4;
+var minuteHandBold = size / 80;
+var minuteHandWidth = size / 2.8;
+var secondHandBold = size / 140;
+var secondHandWidth = size / 2.5;
 
-    var clockCenterX = clock.offsetWidth / 2;
-    var clockCenterY = clock.offsetHeight / 2;
+var doc = document;
+var container = doc.body;
 
-    var angle = 30;
-    var sizeHour = size / 15;
-    var radiusHour = size * 0.42;
+container.appendChild(generateClock());
 
-    for (var i = 1; i < 13; i++) {
-      var sectionCenterX = clockCenterX + radiusHour * Math.sin((angle) / 180 * Math.PI);
-      var sectionCenterY = clockCenterY - radiusHour * Math.cos((angle) / 180 * Math.PI);
+function generateClock() {
+  var base = doc.createElement('div');
+  base.style.position = 'relative';
+  base.style.width = size + 'px';
+  base.style.height = size + 'px';
+  base.style.backgroundColor = baseColor;
+  base.style.borderRadius = '50%';
+  base.appendChild(createClockFace());
+  base.appendChild(createDigitalWatch());
+  base.appendChild(createArrow('hourHand', hourHandWidth, hourHandBold));
+  base.appendChild(createArrow('minuteHand', minuteHandWidth, minuteHandBold));
+  base.appendChild(createArrow('secondHand', secondHandWidth, secondHandBold));
 
-      var section = doc.createElement('div');
-      section.style.position = 'absolute';
-      section.style.width = sizeHour + 'px';
-      section.style.height = sizeHour + 'px';
-      section.style.backgroundColor = sectionColor;
-      section.style.borderRadius = '50%';
-      clock.appendChild(section);
-      section.style.left = Math.round(sectionCenterX - section.offsetWidth / 2) + 'px';
-      section.style.top = Math.round(sectionCenterY - section.offsetHeight / 2) + 'px';
+  return base;
+}
 
-      var hour = doc.createElement('p');
-      hour.style.position = 'absolute';
-      hour.style.margin = '0';
-      hour.style.top = '50%';
-      hour.style.left = '50%';
-      hour.style.transform = 'translate(-50%, -50%)';
-      hour.textContent = i;
-      section.appendChild(hour);
+function createClockFace() {
+  let clockFace = document.createDocumentFragment();
+  var angle = 30;
 
-      angle += 30;
-    }
-
-    var digitalWatchWidth = size / 1.7;
-    var digitalWatchHeight = size / 5;
-
-    var digitalWatch = doc.createElement('div');
-    digitalWatch.style.position = 'absolute';
-    digitalWatch.style.width = digitalWatchWidth + 'px';
-    digitalWatch.style.height = digitalWatchHeight + 'px';
-    clock.appendChild(digitalWatch);
-
-    digitalWatch.style.left = clockCenterX - digitalWatchWidth / 2 + 'px';
-    digitalWatch.style.top = clockCenterY - digitalWatchHeight * 1.4 + 'px';
-
-    var digitalTime = doc.createElement('p');
-    digitalTime.style.textAlign = 'center';
-    digitalTime.style.margin = 'auto';
-    digitalTime.style.fontSize = size / 10 + 'px';
-    digitalTime.style.color = sectionColor;
-    digitalTime.id = 'digital';
-    digitalWatch.appendChild(digitalTime);
-    digitalTime.textContent = '00:00:00';
-
-    var handMargin = size / 40;
-
-    var hourHand = doc.createElement('div');
-    var hourHandBold = size / 50;
-    hourHand.id = 'hourHand';
-    hourHand.style.width = size / 4 + 'px';
-    hourHand.style.border = hourHandBold + 'px solid black';
-    hourHand.style.borderRadius = hourHandBold + 'px';
-    hourHand.style.position = 'absolute';
-    hourHand.style.top = clockCenterY - hourHandBold  + 'px';
-    hourHand.style.left = clockCenterX - handMargin + 'px';
-    hourHand.style.transformOrigin = handMargin + 'px';
-    clock.appendChild(hourHand);
-
-    var minuteHand = doc.createElement('div');
-    var minuteHandBold = size / 80;
-    minuteHand.id = 'minuteHand';
-    minuteHand.style.width = size / 2.8 + 'px';
-    minuteHand.style.border = minuteHandBold + 'px solid black';
-    minuteHand.style.borderRadius = minuteHandBold + 'px';
-    minuteHand.style.position = 'absolute';
-    minuteHand.style.top = clockCenterY - minuteHandBold  + 'px';
-    minuteHand.style.left = clockCenterX - handMargin + 'px';
-    minuteHand.style.transformOrigin = handMargin + 'px';
-    clock.appendChild(minuteHand);
-
-    var secondHand = doc.createElement('div');
-    var secondHandBold = size / 140;
-    secondHand.id = 'secondHand';
-    secondHand.style.width = size / 2.5 + 'px';
-    secondHand.style.border = secondHandBold + 'px solid black';
-    secondHand.style.borderRadius = secondHandBold + 'px';
-    secondHand.style.position = 'absolute';
-    secondHand.style.top = clockCenterY - secondHandBold  + 'px';
-    secondHand.style.left = clockCenterX - handMargin + 'px';
-    secondHand.style.transformOrigin = handMargin + 'px';
-    clock.appendChild(secondHand);
+  for (var i = 1; i < 13; i++) {
+    var sectionCenterX = size / 2 + radiusHour * Math.sin((i * angle) / 180 * Math.PI);
+    var sectionCenterY = size / 2 - radiusHour * Math.cos((i * angle) / 180 * Math.PI);
+    clockFace.appendChild(createHourCircle(sectionCenterX, sectionCenterY, i));
   }
+  return clockFace;
+}
 
-  var doc = document;
-  var container = doc.body;
-  generateClock(container, 350, '#03a9f4', 'red');
-})();
+function createHourCircle(circleX, circleY, number){
+  var section = doc.createElement('div');
+  section.style.position = 'absolute';
+  section.style.width = sizeCircle + 'px';
+  section.style.height = sizeCircle + 'px';
+  section.style.backgroundColor = sectionColor;
+  section.style.borderRadius = '50%';
+  section.style.left = Math.round(circleX - sizeCircle / 2) + 'px';
+  section.style.top = Math.round(circleY - sizeCircle / 2) + 'px';
+
+  var hour = doc.createElement('p');
+  hour.style.position = 'absolute';
+  hour.style.margin = '0';
+  hour.style.top = '50%';
+  hour.style.left = '50%';
+  hour.style.transform = 'translate(-50%, -50%)';
+  hour.textContent = number;
+  section.appendChild(hour);
+  return section;
+}
+
+function createDigitalWatch() {
+  var digitalWatch = doc.createElement('div');
+  digitalWatch.style.position = 'absolute';
+  digitalWatch.style.width = digitalWatchWidth + 'px';
+  digitalWatch.style.height = digitalWatchHeight + 'px';
+
+  digitalWatch.style.left = size / 2 - digitalWatchWidth / 2 + 'px';
+  digitalWatch.style.top = size / 2 - digitalWatchHeight * 1.4 + 'px';
+
+  var digitalTime = doc.createElement('p');
+  digitalTime.style.textAlign = 'center';
+  digitalTime.style.margin = 'auto';
+  digitalTime.style.fontSize = size / 10 + 'px';
+  digitalTime.style.color = sectionColor;
+  digitalTime.id = 'digital';
+  digitalWatch.appendChild(digitalTime);
+  digitalTime.textContent = '00:00:00';
+
+  return digitalWatch;
+}
+
+function createArrow(arrowType, arrowWidth, arrowBold) {
+  var arrow = doc.createElement('div');
+
+  arrow.id = arrowType;
+  arrow.style.width = arrowWidth + 'px';
+  arrow.style.border = arrowBold + 'px solid black';
+  arrow.style.borderRadius = arrowBold + 'px';
+  arrow.style.position = 'absolute';
+  arrow.style.top = size / 2 - arrowBold + 'px';
+  arrow.style.left = size / 2 - handMargin + 'px';
+  arrow.style.transformOrigin = handMargin + 'px';
+
+  return arrow;
+}
